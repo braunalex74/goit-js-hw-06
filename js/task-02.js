31a4cdf8f50f75c7d105a16d8851bf7f6e21a931
braunalex74 <braunalex74@googlemail.com>
2023-02-27 22:00:41 +0200

    DZ_6_3

--- js/task-02.js
+++ js/task-02.js
@@ -10,22 +10,29 @@ const ingredients = [
 const listIngr = document.querySelector("#ingredients");
 
 // const elements = [];
-
 // for (let i = 0; i < ingredients.length; i += 1) {
 //   const titleEl = document.createElement("li");
 //   titleEl.classList.add("item");
 //   titleEl.textContent = ingredients[i];
 //   elements.push(titleEl);
 // }
+// listIngr.append(...elements);
 
-const elements = ingredients.map((option) => {
-  const titleEl = document.createElement("li");
-  titleEl.classList.add("item");
-  titleEl.textContent = option;
+// const elements = ingredients.map((option) => {
+//   const titleEl = document.createElement("li");
+//   titleEl.classList.add("item");
+//   titleEl.textContent = option;
+//   return titleEl;
+// });
+// listIngr.append(...elements);
 
-  return titleEl;
-});
-
-console.log(elements);
-
+const makeIngredients = (options) => {
+  return options.map((option) => {
+    const titleEl = document.createElement("li");
+    titleEl.classList.add("item");
+    titleEl.textContent = option;
+    return titleEl;
+  });
+};
+const elements = makeIngredients(ingredients);
 listIngr.append(...elements);
