ebac1a8337a9ab80552afd222695c82401b79064
braunalex74 <braunalex74@googlemail.com>
2023-03-02 22:23:13 +0200

    DZ_6_4

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,37 @@
-const btnDecrement = document.querySelector("[data-action='decrement']");
-// const btnIncrement = document.querySelector(".increment");
+let btnDecrement = document.querySelector("[data-action='decrement']");
+let btnIncrement = document.querySelector("[data-action='increment']");
+let countNumber = document.querySelector("value");
 
-btnDecrement.addEventListener("click", () => {
-  console.log("Button was clicked");
-});
+// // {/* <div id="counter">
+// <input type="button" id="buttonCountPlus" value="+">
+// <div id="buttonCountNumber">1</div>
+// <input type="button" id="buttonCountMinus" value="-">
+// </div>
 
-btnDecrement.addEventListener("click", handleClick);
-// btnIncrement.addEventListener("click", handleClick);
+// <div id="calculation">150</div> */}
+
+// let calculate = document.getElementById("calculation");
+// let count = document.getElementById("buttonCountNumber");
+// calculation = document.getElementById("calculation").innerHTML;
+
+// document.getElementById("buttonCountPlus").onclick = function() {
+//   let countPlus = count.innerHTML;
+//   if(+countPlus <= 3){
+//     count.innerHTML++;
+//     let countPlus = count.innerHTML;
+//     calculate.innerHTML = calculations(countPlus) ;
+//   }
+// }
+
+// document.getElementById("buttonCountMinus").onclick = function() {
+//   let countMinus = count.innerHTML;
+//   if(+countMinus >= 2){
+//     count.innerHTML--;
+//     let countMinus = count.innerHTML;
+//     calculate.innerHTML = calculations(countMinus) ;
+//   }
+// }
+
+// calculations = (count) => {
+//   return calculation+` * ${count} = ` + (+count)*(+calculation);
+// }
