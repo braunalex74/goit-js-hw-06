fd798018e508f67805ab134afa401bcb00d0232a
braunalex74 <braunalex74@googlemail.com>
2023-03-03 21:43:26 +0200

    DZ_6_5

--- js/task-04.js
+++ js/task-04.js
@@ -2,36 +2,14 @@ let btnDecrement = document.querySelector("[data-action='decrement']");
 let btnIncrement = document.querySelector("[data-action='increment']");
 let countNumber = document.querySelector("value");
 
-// // {/* <div id="counter">
-// <input type="button" id="buttonCountPlus" value="+">
-// <div id="buttonCountNumber">1</div>
-// <input type="button" id="buttonCountMinus" value="-">
-// </div>
+let counterValue = 0;
 
-// <div id="calculation">150</div> */}
+btnDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
 
-// let calculate = document.getElementById("calculation");
-// let count = document.getElementById("buttonCountNumber");
-// calculation = document.getElementById("calculation").innerHTML;
-
-// document.getElementById("buttonCountPlus").onclick = function() {
-//   let countPlus = count.innerHTML;
-//   if(+countPlus <= 3){
-//     count.innerHTML++;
-//     let countPlus = count.innerHTML;
-//     calculate.innerHTML = calculations(countPlus) ;
-//   }
-// }
-
-// document.getElementById("buttonCountMinus").onclick = function() {
-//   let countMinus = count.innerHTML;
-//   if(+countMinus >= 2){
-//     count.innerHTML--;
-//     let countMinus = count.innerHTML;
-//     calculate.innerHTML = calculations(countMinus) ;
-//   }
-// }
-
-// calculations = (count) => {
-//   return calculation+` * ${count} = ` + (+count)*(+calculation);
-// }
+btnIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputtxt = document.querySelector("#name-input");
+const outputtxt = document.querySelector("#name-output");
+
+inputtxt.addEventListener("input", handleInputChange);
+
+function handleInputChange(event) {
+  outputtxt.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.trim() === "") {
+    outputtxt.textContent = "Anonymous";
+  }
+}
