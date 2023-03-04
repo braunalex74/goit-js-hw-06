3159d2fb19f7cc71cb974ede43eb5bf579affa9a
braunalex74 <braunalex74@googlemail.com>
2023-03-04 17:04:36 +0200

    DZ_6_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
 const inputLoginFrom = document.querySelector(".login-form");
+inputLoginFrom.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { login, password },
+  } = event.currentTarget;
+}
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password }
+//   } = event.currentTarget;
+
+//   if (login.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
