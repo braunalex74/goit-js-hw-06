33fdc59cf093577d7a4eb77da7cc3d84621fc876
braunalex74 <braunalex74@googlemail.com>
2023-03-06 22:02:10 +0200

    DZ_6_10

--- js/task-10.js
+++ js/task-10.js
@@ -4,4 +4,13 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const createBtn = document.querySelector("#boxes");
+const handleIdControls = document.querySelector("#constrols");
+const handleNumber = document.querySelector('[type="number"]');
+const handleCreateBtn = document.querySelector("button[data-create]");
+const handleDestroyBtn = document.querySelector("button[data-destroy]");
+const handleIdBoxes = document.querySelector("#boxes");
+
+handleIdBoxes.style.display = "flex";
+handleIdBoxes.style.flexWrap = "wrap";
+handleIdBoxes.style.alignItems = "center";
+handleIdBoxes.style.marginTop = "30px";
